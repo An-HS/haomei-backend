614791510aad86487e01afdcb087a7dd842e5bef
An-HS <zxc5894265@gmail.com>
2025-04-20 01:06:36 +0800

    fix: fix file path

--- firebase_init.py
+++ firebase_init.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, db
 
 # 初始化 Firebase
-cred = credentials.Certificate("firebase_key.json")  # 金鑰檔放這裡
+cred = credentials.Certificate("/etc/secrets/firebase_key.json")  # 金鑰檔放這裡
 firebase_admin.initialize_app(cred, {
     "databaseURL": "https://wetland-guild-default-rtdb.asia-southeast1.firebasedatabase.app/"  # 改成你的專案網址
 })
@@ -13,3 +13,6 @@ def save_checkin(user_id, station_name):
         "station": station_name,
         "timestamp": db.SERVER_TIMESTAMP
     })
+    # 標記這個站已經打過
+    ref_simple = db.reference(f"/checkins_simple/{user_id}")
+    ref_simple.update({station_name: True})
