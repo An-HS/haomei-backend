0ad32d4c43b874b416efbfbb343f6afb7c4ce981
An-HS <zxc5894265@gmail.com>
2025-04-19 23:52:07 +0800

    add: new testing location

--- verify_location.py
+++ verify_location.py
@@ -6,7 +6,9 @@ verify_bp = Blueprint('verify', __name__)
 
 # 預設站點清單（站名 + 經緯度）
 stations = {
-    "安珩家": (23.68219, 120.47902),
+    "大潤發": (23.68318, 120.47792),
+    "嘉義大學民雄校區": (23.54537, 120.42856),
+    "嘉義大學蘭潭校區": (23.46985, 120.45833),
     "好美船屋": (23.48720, 120.16255),
     "好美里3D彩繪村": (23.48591, 120.16214),
     "好美苗圃": (23.48830, 120.16021),
@@ -18,19 +20,15 @@ stations = {
 @verify_bp.route("/verify-location", methods=["POST", "OPTIONS"])
 def verify_location():
     if request.method == "OPTIONS":
-        #print("收到預檢請求 OPTIONS（這裡不會有 userId）")
         return '', 200
   
     data = request.get_json()
-    #print(f"📦 原始資料內容：{data}")
     user_id = data.get("userId")
     user_coords = (data.get("latitude"), data.get("longitude"))
     
-    #print(f"🆔 收到打卡請求：UserID={user_id}，位置=({user_coords})")
 
     for station_name, station_coords in stations.items():
         distance = geodesic(user_coords, station_coords).meters
-        #print(f"使用者與 {station_name} 的距離：{distance:.2f} 公尺")  # debug用
         if distance <= 500:  # 判斷 500 公尺內
             save_checkin(user_id, station_name)  # 儲存到 Firebase
             return jsonify({
