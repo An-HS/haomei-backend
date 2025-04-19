c9a04a54269f671ab546338cca1138f590d65c71
An-HS <zxc5894265@gmail.com>
2025-04-20 01:17:44 +0800

    fix error: module 'firebase_admin.db' has no attribute 'SERVER_TIMESTAMP'

--- firebase_init.py
+++ firebase_init.py
@@ -1,5 +1,6 @@
 import firebase_admin
 from firebase_admin import credentials, db
+from datetime import datetime
 
 # 初始化 Firebase
 cred = credentials.Certificate("/etc/secrets/firebase_key.json")  # 金鑰檔放這裡
@@ -11,7 +12,7 @@ def save_checkin(user_id, station_name):
     ref = db.reference(f"/checkins/{user_id}")
     ref.push({
         "station": station_name,
-        "timestamp": db.SERVER_TIMESTAMP
+        "timestamp": datetime.now().isoformat()  # 儲存為 ISO 格式字串
     })
     # 標記這個站已經打過
     ref_simple = db.reference(f"/checkins_simple/{user_id}")
